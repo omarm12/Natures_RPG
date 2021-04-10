6b2cd541ba042ffa6f5d25ecaca3b009c91f3a6b
Kai G Vilbig <51008173+KaiGVilbig@users.noreply.github.com>
2021-04-10 17:15:39 -0400

    open wiki info in new tab
    get wiki link from pulling specific observation's data, then putting the link into a button to be opened in a new tab on click

---
+++ eduInfo/App.js
@@ -0,0 +1,14 @@
+import { findRenderedComponentWithType } from 'react-dom/test-utils';
+import './App.css';
+import GetObsdata from './getObsdata.js'
+
+function App() {
+
+  return (
+    <div className="App">
+      <GetObsdata />
+    </div>
+  );
+}
+
+export default App;
