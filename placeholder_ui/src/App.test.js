5b67550f5918c13e750560550f2cc037b23d744f
Kai G Vilbig <51008173+KaiGVilbig@users.noreply.github.com>
2021-04-20 16:47:22 -0400

    more specific test for story

--- placeholder_ui/src/App.test.js
+++ placeholder_ui/src/App.test.js
@@ -3,28 +3,10 @@ import { shallow, mount } from "enzyme";
 import App from "./App";
 import LoadingIcon from './components/img/catLoadingIcon.gif'
 import Observations from './components/Observations'
-import GetObsdata from './components/getObsdata'
+import GetObsdata from './components/getObsdatas'
 
 it("Renders without crashing", () => {
   shallow(<App />);
-});
-
-it("Renders Observations header", () => {
-  const wrapper = shallow(<App />);
-  const app = <h1 style={{ paddingBottom: "20px", paddingTop: "60px", paddingLeft: "10px"}}>Observations</h1>;
-  const loading = <div>
-  <img className="loading" src={LoadingIcon} alt="Loading..."/>
-</div>;
-  expect(wrapper.contains(app) || wrapper.contains(loading)).toEqual(true);
-});
-
-it("Renders loading state", () => {
-  const wrapper = shallow(<App />);
-  const app = <div className="Observation" style={{ paddingBottom: "30px"}}></div>;
-  const loading = <div>
-  <img className="loading" src={LoadingIcon} alt="Loading..."/>
-</div>;
-  expect(wrapper.contains(loading)).toEqual(true);
 });
 
 it("Renders eduLink button", () => {
