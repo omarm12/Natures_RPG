6ee0f10980022c5a32a8757fc9c3ba93a708c77c
Cameron Miller <cameron@danicam.net>
2021-05-06 15:12:34 -0400

    Added welcome page, and rebuilt

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,28 +1,37 @@
 import './App.css';
 import Mobile from './pages/Mobile';
 import Desktop from './pages/Desktop';
+import Welcome from './pages/Welcome';
 import {Responsive} from './components/Responsive';
 
 
 
 function App() {
-  return (
 
-    <>
-      <Responsive displayIn={["Mobile"]}>
-        <Mobile/>
-      </Responsive>
-      {/*
-      <Responsive displayIn={["Tablet"]}>
-        <Tablet/>
-      </Responsive>
-      */}
-      <Responsive displayIn={["Tablet", "Laptop"]}>
-        <Desktop/>
-      </Responsive>
-    </>
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const u_id = urlParams.get('u');
 
-  );
+  if(!u_id){
+    return <Welcome/>
+  } else {
+    return (
+      <>
+        <Responsive displayIn={["Mobile"]}>
+          <Mobile/>
+        </Responsive>
+        {/*
+        <Responsive displayIn={["Tablet"]}>
+          <Tablet/>
+        </Responsive>
+        */}
+        <Responsive displayIn={["Tablet", "Laptop"]}>
+          <Desktop/>
+        </Responsive>
+      </>
+
+    );
+  }
 }
 
 export default App;
