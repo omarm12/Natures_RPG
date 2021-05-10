3d9d57ceaf48f86da248fa854e5a50812a62cafc
Cameron Miller <cameron@danicam.net>
2021-05-10 16:23:48 -0400

    commit message

--- web-app/src/components/eduInfo.js
+++ web-app/src/components/eduInfo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './eduInfo.css'
+import './EduInfo.css'
 
 function GetObsdata(props) {
 
@@ -14,4 +14,4 @@ function GetObsdata(props) {
     )
   }
   
-  export default GetObsdata;
+export default GetObsdata;
